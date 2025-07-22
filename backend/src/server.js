f90802c080e429ac4418d88f3531b9e71a44ed1c
Atharva Ghongade <atharvaghongade22@gmail.com>
2025-07-23 00:54:27 +0530

    preparing for deploment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js"
@@ -8,21 +9,34 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 // middleware
 
 // the cors function can also have no arguments, that basically allows requests all ips / domains
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+// only needed if a service is requested from another domain or ip
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+})
+}
 
 /*   
 What is an endpoint? 
